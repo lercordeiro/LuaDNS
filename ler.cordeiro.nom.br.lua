14b60530f43722587d79c2963051298118608317
Luiz Eduardo Roncato Cordeiro <lercordeiro@ler.cordeiro.nom.br>
2025-11-25 12:05:48 -0300

    matei o dcp em chinês

--- ler.cordeiro.nom.br.lua
+++ ler.cordeiro.nom.br.lua
@@ -23,7 +23,6 @@ a("mm", "186.233.20.223")
 a("stats", "186.233.20.223")
 a("repo", "186.233.20.223")
 a("dcp", "186.233.20.223")
-a("xn--i6qq76dy6qfzg", "186.233.20.223")
 a("mail", "186.233.20.223")
 
 -- AAAA records
@@ -35,7 +34,6 @@ aaaa("mm", "2804:814:801a:1000:face::1")
 aaaa("stats", "2804:814:801a:1000:face::1")
 aaaa("repo", "2804:814:801a:1000:face::1")
 aaaa("dcp", "2804:814:801a:1000:face::1")
-aaaa("xn--i6qq76dy6qfzg", "2804:814:801a:1000:face::1")
 aaaa("mail", "2804:814:801a:1000:face::2")
 
 -- SRV records
@@ -44,7 +42,6 @@ aaaa("mail", "2804:814:801a:1000:face::2")
 srv("_https._tcp", "ler.cordeiro.nom.br", 443)
 srv("_https._tcp", "repo.ler.cordeiro.nom.br", 443)
 srv("_https._tcp", "dcp.ler.cordeiro.nom.br", 443)
-srv("_https._tcp", "xn--i6qq76dy6qfzg.ler.cordeiro.nom.br", 443)
 
 -- Matrix/Synapse
 srv("_matrix._tcp", "matrix.ler.cordeiro.nom.br", 8448)
@@ -91,7 +88,6 @@ tlsa("*._tcp.matrix", 2, "d016e1fe311948aca64f2de44ce86c9a51ca041df6103bb52a88eb
 tlsa("*._tcp.mm", 2, "885bf0572252c6741dc9a52f5044487fef2a93b811cdedfad7624cc283b7cdd5", 1, 1)
 tlsa("*._tcp.repo", 2, "d016e1fe311948aca64f2de44ce86c9a51ca041df6103bb52a88eb3f761f57d7", 1, 1)
 tlsa("*._tcp.stats", 2, "3586d4ecf070578cbd27aedce20b964e48bc149faeb9dad72f46b857869172b8", 1, 1)
-tlsa("*._tcp.xn--i6qq76dy6qfzg", 2, "cbbc559b44d524d6a132bdac672744da3407f12aae5d5f722c5f6c7913871c75", 1, 1)
 tlsa("*._tcp.impressora", 2, "2bbad93ab5c79279ec121507f272cbe0c6647a3aae52e22f388afab426b4adba", 1, 1)
 
 -- NÃO ESQUECER!!!  https://www.huque.com/bin/gen_tlsa -> 3 1 1
@@ -104,7 +100,6 @@ tlsa("*._tcp.matrix", 3, "96156a7e0c03881b94ef9954bc06a11d06a4f4e549bd452e1cbaf7
 tlsa("*._tcp.mm", 3, "cec3260506fd5196989bd3a6fda6e4c6ff16ec4d59aa66c0765ac5f299d5285f", 1, 1)
 tlsa("*._tcp.repo", 3, "1e23e5efb32196f949a7cfea858ea2393dfe1341f4671878c83e10ad539ff38a", 1, 1)
 tlsa("*._tcp.stats", 3, "31ff18bc6cd4c0028baca6c0bcbee295655a88f5efa3bf13e68fbcef3213f5f5", 1, 1)
-tlsa("*._tcp.xn--i6qq76dy6qfzg", 3, "4e446ee4ab07512c46da939d20f5878bd09bd472ad5e7a2b0548950d81bada94", 1, 1)
 tlsa("*._tcp.impressora", 3, "45f4923b5cfe8f76f5596e9b87531c431d67a85a0e94757b9e5e1e82acf116d2", 1, 1)
 
 
