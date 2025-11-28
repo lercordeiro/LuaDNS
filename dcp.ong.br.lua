9f0296c719d074b10c33d66e4a49272f9ee151ac
Luiz Eduardo Roncato Cordeiro <lercordeiro@ler.cordeiro.nom.br>
2025-11-28 11:40:38 -0300

    adicionei E7 E8 e E9 aos TLSA

--- dcp.ong.br.lua
+++ dcp.ong.br.lua
@@ -36,10 +36,11 @@ caa("dcp.ong.br", "mailto:csirt@ler.cordeiro.nom.br", "iodef")
 -- Let's Encrypt E8: 885bf0572252c6741dc9a52f5044487fef2a93b811cdedfad7624cc283b7cdd5
 -- Let's Encrypt E9: f1440a9b76e1e41e53a4cb461329bf6337b419726be513e42e19f1c691c5d4b2
 tlsa("*._tcp", 2, "cbbc559b44d524d6a132bdac672744da3407f12aae5d5f722c5f6c7913871c75", 1, 1)
+tlsa("*._tcp", 2, "885bf0572252c6741dc9a52f5044487fef2a93b811cdedfad7624cc283b7cdd5", 1, 1)
+tlsa("*._tcp", 2, "f1440a9b76e1e41e53a4cb461329bf6337b419726be513e42e19f1c691c5d4b2", 1, 1)
 
 -- NÃO ESQUECER!!!  https://www.huque.com/bin/gen_tlsa -> 3 1 1
 -- openssl x509 -in /usr/local/etc/letsencrypt/live/dcp.ong.br/cert.pem -noout -pubkey | openssl pkey -pubin -outform DER | openssl dgst -sha256 
 tlsa("*._tcp", 3, "699e2c1bc5d9b3b4c038200c9da56054026b3586d3cc61505cd82ccb154c299a", 1, 1)
-
 
 -- EOF --
